59dc027b5250a979d86741833b542b784a959674
张志杰 <zj6897@qq.com>
2019-09-24 14:33:40 +0800

    first commit

---
+++ src/components/Login.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        用户名: <input type="text" v-model="loginForm.username" placeholder="请输入用户名">
+        <br><br>
+        密码: <input type="text" v-model="loginForm.password" placeholder="请输入密码">
+        <br><br>
+      <button v-on:click="login">登录</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Login",
+        data(){
+          return{
+            loginForm:{
+              username:"",
+              password:""
+            },
+            responseResult:[]
+
+          }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
